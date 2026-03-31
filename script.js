c008c54e6366fc2a3dff1b5762ba00361399dd98
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-03-31 18:06:25 +0200

    Center and display game cards using Flexbox

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import {games} from './gamesDB.js';
 const cards=document.getElementById("cards");
 for (let i = 0; i < games.length; i++) {
   cards.innerHTML += `
-    <div class="bg-white p-4 rounded-xl shadow w-64">
+    <div class="bg-white p-4 rounded-xl shadow w-64  ">
       <img src="${games[i].image}" class="w-full h-40 object-cover rounded-lg mb-2">
       <h2 class="font-bold text-lg">${games[i].title}</h2>
       <p class="text-gray-500">${games[i].category}</p>

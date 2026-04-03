378c4539dc7174bacd6b79d2a3e5d5dc1f0701f5
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-04-03 11:42:57 +0200

    add game filtering by category

--- script.js
+++ script.js
@@ -36,7 +36,178 @@ searchInput.addEventListener("input", (e) => {
   }
 });
 
-function filterCategory(cat) {
-  const filtered = games.filter(game => game.category === cat);
-  displayGames(filtered);
+
+
+
+let RPG=document.getElementById("RPG");
+RPG.onclick=function(){
+  RPGcategory();
 }
+function RPGcategory(){
+  cards.innerHTML="";
+  let found=false;
+  for(let i=0 ; i<games.length;i++){
+    if("RPG"==games[i].category){
+      found=true;
+      cards.innerHTML += `
+    <div class="bg-white p-4 rounded-xl shadow lg:w-64 w-[90%]  ">
+      <img src="${games[i].image}" class="w-full h-40 object-cover rounded-lg mb-2">
+      <div  class="flex justify-between items-center mb-1">
+      <h2 class="font-bold text-lg">${games[i].title}</h2>
+      <button onclick="addToCart('${games[i].title}')" class="mt-2">
+        <img src="photos/ajouter-au-panier.png" class="w-6 h-6">
+      </button>
+      
+      </div>
+      <p class="text-gray-500">${games[i].category}</p>
+      <p class="text-blue-600 font-semibold">${games[i].price}$</p>
+       
+          </div>
+  `;
+    }
+  }
+}
+
+let Action=document.getElementById('Action');
+Action.onclick=function(){
+  Actioncategory();
+}
+function Actioncategory(){
+  cards.innerHTML="";
+  let found=false;
+  for(let i=0;i<games.length;i++){
+    if("Action"==games[i].category){
+      found=true;
+       cards.innerHTML += `
+    <div class="bg-white p-4 rounded-xl shadow lg:w-64 w-[90%]  ">
+      <img src="${games[i].image}" class="w-full h-40 object-cover rounded-lg mb-2">
+      <div  class="flex justify-between items-center mb-1">
+      <h2 class="font-bold text-lg">${games[i].title}</h2>
+      <button onclick="addToCart('${games[i].title}')" class="mt-2">
+        <img src="photos/ajouter-au-panier.png" class="w-6 h-6">
+      </button>
+      
+      </div>
+      <p class="text-gray-500">${games[i].category}</p>
+      <p class="text-blue-600 font-semibold">${games[i].price}$</p>
+       
+          </div>
+  `;
+
+    }
+  }
+}
+
+FPS.onclick=function(){
+  FPScategory();
+}
+function FPScategory(){
+  cards.innerHTML="";
+  let found =false;
+  for(let i =0 ;i<games.length;i++){
+    if("FPS"==games[i].category){
+      found=true;
+       cards.innerHTML += `
+    <div class="bg-white p-4 rounded-xl shadow lg:w-64 w-[90%]  ">
+      <img src="${games[i].image}" class="w-full h-40 object-cover rounded-lg mb-2">
+      <div  class="flex justify-between items-center mb-1">
+      <h2 class="font-bold text-lg">${games[i].title}</h2>
+      <button onclick="addToCart('${games[i].title}')" class="mt-2">
+        <img src="photos/ajouter-au-panier.png" class="w-6 h-6">
+      </button>
+      
+      </div>
+      <p class="text-gray-500">${games[i].category}</p>
+      <p class="text-blue-600 font-semibold">${games[i].price}$</p>
+       
+          </div>
+  `;
+    }
+  }
+
+}
+
+All.onclick=function(){
+  cards.innerHTML="";
+
+for(let i = 0; i < games.length; i++){
+   cards.innerHTML += `
+    <div class="bg-white p-4 rounded-xl shadow lg:w-64 w-[90%]  ">
+      <img src="${games[i].image}" class="w-full h-40 object-cover rounded-lg mb-2">
+      <div  class="flex justify-between items-center mb-1">
+      <h2 class="font-bold text-lg">${games[i].title}</h2>
+      <button onclick="addToCart('${games[i].title}')" class="mt-2">
+        <img src="photos/ajouter-au-panier.png" class="w-6 h-6">
+      </button>
+      
+      </div>
+      <p class="text-gray-500">${games[i].category}</p>
+      <p class="text-blue-600 font-semibold">${games[i].price}$</p>
+       
+          </div>
+  `;
+}
+}
+let Sport=document.getElementById("Sport");
+Sport.onclick=function(){
+  Sportcategory();
+}
+function Sportcategory(){
+  cards.innerHTML="";
+  let found=false;
+  for(let i=0 ; i<games.length;i++){
+    if("Sport"==games[i].category){
+      found=true;
+      cards.innerHTML += `
+    <div class="bg-white p-4 rounded-xl shadow lg:w-64 w-[90%]  ">
+      <img src="${games[i].image}" class="w-full h-40 object-cover rounded-lg mb-2">
+      <div  class="flex justify-between items-center mb-1">
+      <h2 class="font-bold text-lg">${games[i].title}</h2>
+      <button onclick="addToCart('${games[i].title}')" class="mt-2">
+        <img src="photos/ajouter-au-panier.png" class="w-6 h-6">
+      </button>
+      
+      </div>
+      <p class="text-gray-500">${games[i].category}</p>
+      <p class="text-blue-600 font-semibold">${games[i].price}$</p>
+       
+          </div>
+  `;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export function localitems(){
+  localStorage.setItem("sac" , JSON.stringify(games))
+  }
+  export function getitems(){
+    if(localStorage.getItem("sac")){
+      let games =JSON.parse(localStorage.getItem("sac"))
+    }
+  }
+  

13ac7886bc99097256d5aec81195405064f54210
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-04-10 13:53:15 +0200

    ajouter un file de price

---
+++ priceStats.js
@@ -0,0 +1,10 @@
+import {games} from './gamesDB.js';
+export  function getMostExpensive(games){
+    for(let i=0;i< games.length ;i++){
+        if(games[i].price)
+            {
+
+        }
+
+    }
+}
